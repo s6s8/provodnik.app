eba7bb2c99776002e8861e013eaaa09c1ed72bb9
QuantumBek Orchestrator <dev@rgx.ge>
2026-07-04 03:04:07 +0400

    chore: drop stray .tmp SQL scratch files from PR, gitignore /.tmp/

--- .tmp/check1.sql
+++
@@ -1,1 +0,0 @@
-select verification_status, is_available, (slug is not null and slug <> '') as has_slug, count(*) from public.guide_profiles group by 1,2,3 order by 1;
--- .tmp/check2.sql
+++
@@ -1,1 +0,0 @@
-select version, name from supabase_migrations.schema_migrations order by version desc limit 12;
